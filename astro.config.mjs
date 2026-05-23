2791d689e2da7b08289c98b168c56d782f815d91
yyq <2483387164@qq.com>
2026-05-24 01:13:48 +0800

    Add GitHub Pages deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://yangyuanquan8-dotcom.github.io',
+  base: '/my-blog',
   integrations: [tailwind()],
   markdown: {
     shikiConfig: {
